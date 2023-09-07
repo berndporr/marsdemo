f3f2f87666c73bb386edad7894d60a7488309a5b
Bernd Porr <Bernd.Porr@glasgow.ac.uk>
2023-09-07 21:22:57 +0100

    All modules load but cannot find tf

--- runit.sh
+++ runit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 module load apps/tensorflow
-module load apps/python3/3.11.5/gcc-4.8.5
 module load apps/nvidia-cuda/11.7.1
+module load apps/python3/3.11.5/gcc-4.8.5
 
 echo "Hello"
 
